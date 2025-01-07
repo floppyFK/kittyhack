39b6dad0117252c3896d4ebdb3e36095c10b62c9
Florian Kispert <florian.kispert@googlemail.com>
2025-01-07 22:09:31 +0000

    added favicon

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from shiny import App
 from src.ui import app_ui
 from src.server import server
+import os
 
-app = App(app_ui, server)
+path_www = os.path.join(os.path.dirname(__file__), "www")
+
+app = App(app_ui, server, static_assets={"/": path_www})

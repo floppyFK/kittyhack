a5ba337b6cbb8a844869ab24f2c43f02a5e3e2a1
Florian Kispert <49509007+floppyFK@users.noreply.github.com>
2025-01-09 08:52:54 +0100

    Revert "Full replacement of ai logic"

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from shiny import App
 from src.ui import app_ui
 from src.server import server
-import os
 
-path_www = os.path.join(os.path.dirname(__file__), "www")
-
-app = App(app_ui, server, static_assets={"/": path_www})
+app = App(app_ui, server)
